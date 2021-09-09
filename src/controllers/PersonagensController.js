30b560508b4997edc6c75edc2d1935085320b21f
FIRST_NAME LAST_NAME <filipepaiva457@hotmail.com>
2021-09-09 20:07:57 -0300

    Criação do back

---
+++ src/controllers/PersonagensController.js
@@ -0,0 +1,94 @@
+const Personagem = require("../models/Personagem");
+
+const getAll = async (req, res) => {
+  try {
+    const personagens = await Personagem.find(); // Promisse
+    return res.send({ personagens });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const personagem = await Personagem.findById(id);
+    if (!personagem) {
+      res.status(404).json({ message: "Personagem não encontrado" });
+      return;
+    }
+    return res.send({ personagem });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+};
+
+const create = async (req, res) => {
+  const { nome, tipo, genero, imagem } = req.body;
+
+  if (!nome || !tipo || !genero || !imagem) {
+    res.status(400).send({
+      message: "Você não enviou todos os dados necessários para o cadastro",
+    });
+    return;
+  }
+
+  const novoPersonagem = await new Personagem({
+    nome,
+    tipo,
+    genero,
+    imagem,
+  });
+
+  try {
+    await novoPersonagem.save();
+    return res
+      .status(201)
+      .send({ message: "Personagem criado com sucesso", novoPersonagem });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+};
+
+const update = async (req, res) => {
+  const { nome, tipo, genero, imagem } = req.body;
+
+  if (!nome || !tipo || !genero || !imagem) {
+    res.status(400).send({
+      message: "Você não enviou todos os dados necessários para o cadastro",
+    });
+    return;
+  }
+
+  res.personagem.nome = nome
+  res.personagem.tipo = tipo
+  res.personagem.genero = genero
+  res.personagem.imagem = imagem
+
+  try {
+      await res.personagem.save();
+      res.send({message: "Personagem alterado com sucesso!"})
+
+  } catch (err) {
+      res.status(500).send({ error: err})
+  }
+
+};
+
+const del = async (req, res) => {
+    try {
+        await res.personagem.remove()
+        return res.send({ message: "Personagem removido com sucesso!"})
+    } catch (err) {
+        res.status(500).send({ error: err})
+    }
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  update,
+  del
+};
---
+++ src/models/Personagem.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose');
+
+const personagemSchema = new mongoose.Schema({
+    nome:{
+        type: String,
+        require: true
+    },
+    tipo:{
+        type: String,
+        require: true
+    },
+    genero:{
+        type: String,
+        require: true
+    },
+    imagem:{
+        type: String,
+        require: true
+    }
+})
+
+module.exports = mongoose.model("Personagem", personagemSchema)
